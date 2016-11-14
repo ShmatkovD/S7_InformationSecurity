eaf148f50dfa6e1d01480c667605bd202d265141
smatkovd <client.pas.95@gmail.com>
2016-11-15 00:26:28 +0300

    Added lab 3.

--- l3/task_3.py
+++ l3/task_3.py
@@ -1,10 +1,12 @@
-from __future__ import unicode_literals, absolute_import
+import random
 
 import numpy as np
 
-# AES
+Nk = 8
+Nb = 4
+Nr = 14
 
-Sbox = [
+s_box = (
     0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,
     0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0,
     0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15,
@@ -20,10 +22,10 @@ Sbox = [
     0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a,
     0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e,
     0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf,
-    0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16,
-]
+    0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16
+)
 
-InvSbox = [
+inv_s_box = (
     0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb,
     0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb,
     0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e,
@@ -39,65 +41,199 @@ InvSbox = [
     0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f,
     0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef,
     0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61,
-    0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d,
-]
+    0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d
+)
 
-Rcon = [
-    [0x00, 0x00, 0x00, 0x00],
-    [0x01, 0x00, 0x00, 0x00],
-    [0x02, 0x00, 0x00, 0x00],
-    [0x04, 0x00, 0x00, 0x00],
-    [0x08, 0x00, 0x00, 0x00],
-    [0x10, 0x00, 0x00, 0x00],
-    [0x20, 0x00, 0x00, 0x00],
-    [0x40, 0x00, 0x00, 0x00],
-    [0x80, 0x00, 0x00, 0x00],
-    [0x1b, 0x00, 0x00, 0x00],
-    [0x36, 0x00, 0x00, 0x00],
-]
+r_con = (
+    (0x00, 0x00, 0x00, 0x00),
+    (0x01, 0x00, 0x00, 0x00),
+    (0x02, 0x00, 0x00, 0x00),
+    (0x04, 0x00, 0x00, 0x00),
+    (0x08, 0x00, 0x00, 0x00),
+    (0x10, 0x00, 0x00, 0x00),
+    (0x20, 0x00, 0x00, 0x00),
+    (0x40, 0x00, 0x00, 0x00),
+    (0x80, 0x00, 0x00, 0x00),
+    (0x1b, 0x00, 0x00, 0x00),
+    (0x36, 0x00, 0x00, 0x00)
+)
 
 
-def encrypt_block(input):
-    """Encrypt input with AES algo
-    :type input: str
-    :rtype: str
-    """
-    assert len(input) == 16
-    state = []
+def generate_key():
+    return np.random.randint(0, 0xFF, (Nk, 4))
 
-    nb = 4
-    rows_count = 4
-    rounds_count = 10
 
-    for r in xrange(rows_count):
-        state.append([])
-        for c in xrange(nb):
-            state[r].append(
-                int(input[r + 4 * c])
-            )
+def generate_data():
+    return np.array([random.randint(0, 0xFF) for _ in xrange(4 * Nb)])
 
-    state = np.array(state)
 
-    add_round_key(state)
+def to_bits(b):
+    return map(int, bin(b)[2:])
 
-    for _ in xrange(rounds_count):
-        sub_bytes(state)
-        shift_rows(state)
-        mix_columns(state)
-        add_round_key(state)
 
-    sub_bytes(state)
-    shift_rows(state)
-    add_round_key(state)
+def to_int(bits):
+    return int(''.join(map(str, bits)), 2)
 
-    rows = state.shape[0]
-    cols = state.shape[1]
 
-    output = []
-    for c in xrange(cols):
-        for r in xrange(rows):
-            output.append(
-                str(state[r, c])
-            )
+def add_round_key(state, key):
+    return state ^ key
 
-    return ''.join(output)
+
+sub_bytes = np.vectorize(lambda x: s_box[x])
+inv_sub_bytes = np.vectorize(lambda x: inv_s_box[x])
+
+
+def shift_rows(state):
+    return np.array([np.roll(row, -i) for i, row in enumerate(state)])
+
+
+def inv_shift_rows(state):
+    return np.array([np.roll(row, i) for i, row in enumerate(state)])
+
+
+def g_mul(a, b):
+    p = 0
+    for counter in xrange(8):
+        if b & 1:
+            p ^= a
+        hi_bit_set = a & 0x80
+        a = (a << 1) & 0xFF
+        if hi_bit_set:
+            a ^= 0x1B
+        b >>= 1
+    return p
+
+
+def g_linear(a, b):
+    return reduce(lambda x, y: x ^ y,
+                  [g_mul(*p) for p in zip(a, b)],
+                  0)
+
+
+def mix_columns(state):
+    r = np.copy(state)
+    for i in range(r.shape[1]):
+        a = r[:, i]
+        r[:, i] = [
+            g_linear(a, [2, 3, 1, 1]),
+            g_linear(a, [1, 2, 3, 1]),
+            g_linear(a, [1, 1, 2, 3]),
+            g_linear(a, [3, 1, 1, 2])
+        ]
+    return r
+
+
+def inv_mix_columns(state):
+    r = np.copy(state)
+    for i in range(r.shape[1]):
+        a = r[:, i]
+        r[:, i] = [
+            g_linear(a, [14, 11, 13, 9]),
+            g_linear(a, [9, 14, 11, 13]),
+            g_linear(a, [13, 9, 14, 11]),
+            g_linear(a, [11, 13, 9, 14])
+        ]
+    return r
+
+
+def key_expansion(key):
+    w = key.reshape((Nk, 4))
+
+    for i in xrange(Nk, Nb * (Nr + 1)):
+        temp = w[i - 1]
+        if i % Nk == 0:
+            temp = sub_bytes(np.roll(temp, -1)) ^ r_con[i / Nk]
+        elif Nk > 6 and i % Nk == 4:
+            temp = sub_bytes(temp)
+        w = np.vstack((w, w[i - Nk] ^ temp))
+
+    return w.reshape((Nr + 1, Nb, 4)).transpose(0, 2, 1)
+
+
+def encrypt(inp, w):
+    state = inp.reshape((4, Nb))
+    state = add_round_key(state, w[0])
+    for r in xrange(1, Nr):
+        state = sub_bytes(state)
+        state = shift_rows(state)
+        state = mix_columns(state)
+        state = add_round_key(state, w[r])
+    state = sub_bytes(state)
+    state = shift_rows(state)
+    state = add_round_key(state, w[Nr])
+
+    out = np.reshape(state, 4 * Nb)
+    return out
+
+
+def decrypt(inp, w):
+    state = inp.reshape((4, Nb))
+    state = add_round_key(state, w[Nr])
+    for r in xrange(Nr - 1, 0, -1):
+        state = inv_shift_rows(state)
+        state = inv_sub_bytes(state)
+        state = add_round_key(state, w[r])
+        state = inv_mix_columns(state)
+
+    state = inv_shift_rows(state)
+    state = inv_sub_bytes(state)
+    state = add_round_key(state, w[0])
+
+    out = np.reshape(state, 4 * Nb)
+    return out
+
+
+def test():
+    key = generate_key()
+    w = key_expansion(key)
+    data = generate_data()
+
+    encrypted = encrypt(data, w)
+    decrypted = decrypt(encrypted, w)
+
+    print 'data:\n', data
+    print 'encrypted:\n', encrypted
+    print 'decrypted:\n', decrypted
+    print 'ok' if np.array_equal(data, decrypted) else 'failed'
+
+
+def generate_state():
+    return np.random.randint(0, 0xFF, (4, Nb))
+
+
+def test_funcs(func, inv_func):
+    a = generate_state()
+    b = func(a)
+    c = inv_func(b)
+
+    print 'ok' if np.array_equal(a, c) else 'failed'
+
+
+def test_add_round_key():
+    a = generate_state()
+    key = generate_key()
+    w = key_expansion(key)
+
+    b = add_round_key(a, w[0])
+    c = add_round_key(b, w[0])
+
+    print 'ok' if np.array_equal(a, c) else 'failed'
+
+
+def test_state_funcs():
+    print 'mix_columns:'
+    test_funcs(mix_columns, inv_mix_columns)
+
+    print 'sub_bytes:'
+    test_funcs(sub_bytes, inv_sub_bytes)
+
+    print 'shift_rows:'
+    test_funcs(shift_rows, inv_shift_rows)
+
+    print 'add_round_key:'
+    test_add_round_key()
+
+    print
+
+if __name__ == '__main__':
+    test()
